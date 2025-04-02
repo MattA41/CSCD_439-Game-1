ec897d8baf849f20f9b26819a4bb840cf251034a
Matt <madams41@ewu.edu>
2025-04-02 15:44:00 -0700

    Finished enemy movement

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Vector3.Distance(this.transform.position,waypoints[currentWP].transform.position)< .1f)
+            currentWP++;
         
+        //this.transform.LookAt(waypoints[currentWP].transform);
+        Vector3 newPos = Vector3.MoveTowards(this.transform.position, waypoints[currentWP].transform.position, speed * Time.deltaTime);
+        this.transform.position = newPos;
     }
 }
