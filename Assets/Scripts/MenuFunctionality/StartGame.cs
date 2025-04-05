e4d5626011d9ba0a07d54bd1e5d3033929261814
Colby Crutcher <94940284+colbycrutcher@users.noreply.github.com>
2025-04-05 16:53:12 -0700

    Added images, and fixed menu order

--- Assets/Scripts/MenuFunctionality/StartGame.cs
+++ Assets/Scripts/MenuFunctionality/StartGame.cs
@@ -18,6 +18,6 @@ public class StartGame : MonoBehaviour
     }
 
     public void StartGameButton(){
-        SceneManager.LoadScene("Scenes/Map2");
+        SceneManager.LoadScene("Scenes/Menu/SelectScreen");
     }
 }
