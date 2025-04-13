e8096fc7e6246d61ce4a0993c5546750bdab2619
Matt <madams41@ewu.edu>
2025-04-13 14:48:16 -0700

    fixed wave logic

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -7,26 +7,28 @@ using System.Threading;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab; //prfab for spawning enemys
-    public float spawnDelay = 3f; //delay inbetween enemyies
+    public float spawnDelay = 3.0f; //delay inbetween enemy spawns
     private float nextSpawnTime; //time when next enemy will spawn
     //enemy vars
     public GameObject[] mapWayPoints; //waypoints for enemy navigation
     public PlayerManager pmanager; //player manager for enemy to affect health
-    public int enemyHealth; //holds the health value for enemys
-    public int enemySpeed; //hold the speed value for enemys
+    public int enemyHealth = 100; //holds the health value for enemys
+    public int enemySpeed = 5; //hold the speed value for enemys
     //wave management
     public bool IsWaves; // If the level will have waves set to true
     public int waveNums = 10; //number of waves for the level
     public int enemyStartNum = 10; //number of enemys at start
     public int currWave; //the current waves
-    public int waveDelay = 300000; //delay inbetween waves in milliseconds default is 30 seconds
-    private int currEnemyCount ; //used to add enemys 
+    public int waveDelay = 30; //delay inbetween waves in seconds default is 30 seconds
     public int enemyAdd = 3; //number of enemys to add at end of wave
+    private int currEnemyCount ; //used to add enemys
+    private bool isSpawningWaves = false;
 
     //Start is called before the first frame update
     void Start()
     {
         currEnemyCount = enemyStartNum;
+        
     }
 
     //Update is called once per frame
@@ -34,22 +36,48 @@ public class EnemySpawner : MonoBehaviour
     {
         if (IsWaves)
         {
-            for (int i = 0; i <= waveNums; i++)
+            if(!isSpawningWaves)
             {
-                Debug.Log("wave " + currWave + "started")
-                WaveSpawn();
-                Debug.Log("wave " + currWave + "ended")
-                currEnemyCount = currEnemyCount + enemyAdd;
-                Thread.Sleep(waveDelay);
+                StartCoroutine(WaveSpawner());
             }
             
+        }else
+        {
+            if (Time.time > nextSpawnTime)
+            {
+                SpawnEnemy();
+                nextSpawnTime = Time.time + spawnDelay;
+            }
         }
 
-        if (Time.time > nextSpawnTime)
+        
+    }
+    IEnumerator WaveSpawner() //spawn waves with a wait time without freezing the whole game
+    {
+        isSpawningWaves = true;
+
+        for (int i = 0; i < waveNums; i++)
         {
-            SpawnEnemy();
-            nextSpawnTime = Time.time + spawnDelay;
+            currWave = i + 1;
+            Debug.Log("Wave " + currWave + " started");
+
+            // Spawn all enemies in the wave
+            for (int j = 0; j < currEnemyCount; j++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(spawnDelay);
+            }
+
+            Debug.Log("Wave " + currWave + " ended");
+            currEnemyCount += enemyAdd;
+            Debug.Log("Current enemy count: " + currEnemyCount);
+
+            // Wait before the next wave starts
+            yield return new WaitForSeconds(waveDelay);
         }
+
+        Debug.Log("All waves complete");
+        isSpawningWaves = false;
     }
     //spawns enemy and hands in values
     void SpawnEnemy()
@@ -58,19 +86,9 @@ public class EnemySpawner : MonoBehaviour
         var enemyScript = enemy.GetComponent<Enemy>();
         enemyScript.waypoints = mapWayPoints;
         enemyScript.manager = pmanager;
+        enemyScript.health = enemyHealth;
+        enemyScript.speed = enemySpeed;
     }
 
-    void WaveSpawn() //spawns enemies untill wave count
-    {
-        int i = 0;
-        while (i <= currEnemyCount)
-        {
-            if (Time.time > nextSpawnTime)
-            {
-                SpawnEnemy();
-                nextSpawnTime = Time.time + spawnDelay;
-                i++;
-            }
-        }
-    }
+    
 }
