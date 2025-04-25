b66ded5a19a303f0afabb0ca1f1ba797519d3f3a
Sergei Uss <sashikhmin@ewu.edu>
2025-04-24 18:19:21 -0700

    Map 1 new tower selector

--- Assets/Scripts/TowerSlotDrag.cs
+++ Assets/Scripts/TowerSlotDrag.cs
@@ -3,31 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TowerSlotDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class TowerSlotDrag : MonoBehaviour, IPointerDownHandler
 {
 
     public GameObject towerPrefab;
-
-    private GameObject previewTower;
-
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        previewTower = Instantiate(towerPrefab);
-        previewTower.GetComponent<Collider2D>().enabled = false;
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (previewTower != null)
-        {
-            previewTower.transform.position = mousePos;
-        }
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Destroy(previewTower); // For now, just destroy
-    }
+    public int towerCost;
+    public TowerPlacementManager placementManager;
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		placementManager.SetTowerToPlace(towerPrefab, towerCost);
+	}
 }
