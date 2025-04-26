ac18bf2a1099ca3355ce62b3cf1bf0eeef042e9c
Sergei Uss <sashikhmin@ewu.edu>
2025-04-25 20:26:58 -0700

    Enemy init

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,26 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 10.0f;
-    public PlayerManager manager; 
+    public PlayerManager manager;
     public int health = 100;
     public GameObject[] waypoints;
     int currentWP = 0;
     public int worth = 25;
 
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position,waypoints[currentWP].transform.position)< .1f)
-            currentWP++;
-        
-        Vector3 newPos = Vector3.MoveTowards(this.transform.position, waypoints[currentWP].transform.position, speed * Time.deltaTime);
-        this.transform.position = newPos;
+        if (currentWP >= waypoints.Length) return;
+
+        Vector3 targetPos = waypoints[currentWP].transform.position;
+        Vector3 direction = (targetPos - transform.position).normalized;
+
+        // Set animation parameters
+        animator.SetFloat("moveX", direction.x);
+        animator.SetFloat("moveY", direction.y);
+
+        // Flip for left-facing movement
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            spriteRenderer.flipX = direction.x > 0;
+        }
+
+        // // Flip sprite when walking left
+        // if (direction.x < -0.1f) spriteRenderer.flipX = true;
+        // else if (direction.x > 0.1f) spriteRenderer.flipX = false;
+
+        // Move toward waypoint
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f) currentWP++;
     }
 
     public void TakeDamage(int damage)
@@ -47,8 +69,10 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Goal" && manager.health != 0){
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Goal" && manager.health != 0)
+        {
             manager.health = --manager.health;
             Debug.Log("goal reached " + manager.health);
             Destroy(this.gameObject);
